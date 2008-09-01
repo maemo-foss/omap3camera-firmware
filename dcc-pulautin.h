0e21283de0a402981efbb9790d45d4574f7b7b1c
Sakari Ailus <sakari.ailus@nokia.com>
2008-09-01 12:14:17 +0300

    Do some stuff.
    Signed-off-by: Sakari Ailus <sakari.ailus@nokia.com>

--- dcc-pulautin.h
+++ dcc-pulautin.h
@@ -2,7 +2,6 @@
 
 #define DECLARE_SMIA_REGLIST(_name, _type, _mode, ...)	\
 	static struct smia_reglist _name = {		\
-		.size = sizeof(_name),			\
 		.type = _type,				\
 		.mode = _mode,				\
 		.regs = { __VA_ARGS__ }			\
