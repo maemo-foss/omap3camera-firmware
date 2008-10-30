7be31ba5042078cb0f9a644ef8c7ab4a13556ad7
Tuukka.O Toivonen <tuukka.o.toivonen@nokia.com>
2008-10-30 12:32:32 +0200

    dcc-pulautin: et8ek8: add width/height calculation
    et8ek8: add width/height calculation
    
    Signed-off-by: Tuukka Toivonen <tuukka.o.toivonen@nokia.com>

--- makemodes-et8ek8.pl
+++ makemodes-et8ek8.pl
@@ -35,4 +35,42 @@ sub sensor_pixel_clock {
 	return $spck + 0;
 };
 
+sub sensor_width {
+	my $regsref = shift @_;
+	my %regs = %$regsref;
+
+	my $h_count_lo  = $regs{"0x1220"};
+	my $h_count_hi  = $regs{"0x1221"} & 0x07;
+	my $h_count     = ($h_count_hi<<8) | $h_count_lo;
+
+	return $h_count * 24;
+}
+
+sub sensor_height {
+	my $regsref = shift @_;
+	my %regs = %$regsref;
+
+	my $v_count_lo  = $regs{"0x1222"};
+	my $v_count_hi  = $regs{"0x1223"} & 0x1F;
+	my $v_count     = ($v_count_hi<<8) | $v_count_lo;
+	my $moni_mode   = $regs{"0x121B"} & 0x07;
+	my $pic_size    = ($regs{"0x121B"} >> 4) & 0x07;
+
+	my $binning;
+	$binning = 1/1 if ($moni_mode>=4);
+	$binning = 1/(5 - $moni_mode) if ($moni_mode>=1 && $moni_mode<=3);
+	$binning = 1/6 if ($moni_mode==0);
+
+	my $cropping;
+	$cropping = 1/1 if ($pic_size>=6);
+	$cropping = 3/4 if ($pic_size==5);
+	$cropping = 2/3 if ($pic_size==4);
+	$cropping = 1/2 if ($pic_size==3);
+	$cropping = 1/3 if ($pic_size==2);
+	$cropping = 1/4 if ($pic_size==1);
+	$cropping = 1/6 if ($pic_size==0);
+
+	return $v_count * 24 * $binning * $cropping;
+}
+
 1;
