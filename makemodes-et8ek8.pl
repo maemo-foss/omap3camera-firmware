789c1eaf39da15b3eafaa5cac688218f11119463
Tuukka.O Toivonen <tuukka.o.toivonen@nokia.com>
2008-10-30 13:12:26 +0200

    dcc-pulautin: et8ek8: fix maximum exposure calculation
    et8ek8: fix maximum exposure calculation
    
    Signed-off-by: Tuukka Toivonen <tuukka.o.toivonen@nokia.com>

--- makemodes-et8ek8.pl
+++ makemodes-et8ek8.pl
@@ -4,17 +4,7 @@
 # $modelist_max_exp[$modenum] = sensor_max_exp(\%regs);
 
 sub sensor_max_exp {
-	my $regsref = shift(@_);
-	my %regs = %$regsref;
-	my $v_count_lo  = $regs{"0x1222"};
-	my $v_count_hi  = $regs{"0x1223"} & 0x1F;
-	my $moni_mode   = $regs{"0x121B"} & 0x07;
-	my $v_count     = ($v_count_hi<<8) | $v_count_lo;
-	if ($v_count > 0) {
-		return $v_count*24/(5 - $moni_mode) - 4;
-	}
-
-	return undef;
+	return sensor_height(@_) - 4;
 }
 
 sub sensor_pixel_clock {
