257c3cbfd85c9cbb06b3ba61413ed4de26dc325e
Tuukka.O Toivonen <tuukka.o.toivonen@nokia.com>
2008-12-10 12:59:50 +0200

    vs6555: add pixel clock, fps, and frame size calculations
    vs6555: add pixel clock, fps, and frame size calculations
    
    Signed-off-by: Tuukka Toivonen <tuukka.o.toivonen@nokia.com>

---
+++ makemodes-vs6555.pl
@@ -0,0 +1,58 @@
+# Sensor specific code: vs6555
+
+sub sensor_max_exp {
+	return sensor_height(@_) - 3;
+}
+
+sub sensor_pixel_clock {
+	my $regsref = shift @_;
+	my %regs = %$regsref;
+
+	my $pll_multipler    = ($regs{"0x0306"} << 8) | $regs{"0x0307"};
+	my $pre_pll_clk_div  = ($regs{"0x0304"} << 8) | $regs{"0x0305"};
+	my $vt_sys_clk_div   = ($regs{"0x0302"} << 8) | $regs{"0x0303"};
+	my $vt_pix_clk_div   = ($regs{"0x0300"} << 8) | $regs{"0x0301"};
+
+	return undef if (!$pre_pll_clk_div);
+	return undef if (!$vt_sys_clk_div);
+	return undef if (!$vt_pix_clk_div);
+
+	my $vt_pix_clk_freq = ($extclk * $pll_multipler)
+	                      / ($pre_pll_clk_div * $vt_sys_clk_div * $vt_pix_clk_div);
+	return $vt_pix_clk_freq;
+}
+
+sub sensor_width {
+	my $regsref = shift @_;
+	my %regs = %$regsref;
+
+	my $line_length_pck = ($regs{"0x0342"} << 8) | $regs{"0x0343"};
+
+	return $line_length_pck;
+}
+
+sub sensor_height {
+	my $regsref = shift @_;
+	my %regs = %$regsref;
+
+	my $frame_length_lines = ($regs{"0x0340"} << 8) | $regs{"0x0341"};
+	return $frame_length_lines;
+}
+
+sub sensor_fps {
+	my $regsref = shift @_;
+	my %regs = %$regsref;
+
+	my $line_length_pck    = ($regs{"0x0342"} << 8) | $regs{"0x0343"};
+	my $frame_length_lines = ($regs{"0x0340"} << 8) | $regs{"0x0341"};
+
+	return undef if (!$line_length_pck);
+	return undef if (!$frame_length_lines);
+
+	my $framerate = sensor_pixel_clock($regsref)
+	                / ($line_length_pck * $frame_length_lines);
+
+	return $framerate;
+}
+
+1;
